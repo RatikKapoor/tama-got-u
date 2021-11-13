e572f178507c41cb0f2428712751e384898fd40e
Ratik Kapoor <ratikrk@gmail.com>
2021-11-13 14:35:59 -0700

    feat: Add firebase init and config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Test from "./components/Test";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5kgLAtAFkz_H_JmUVLmh9Bfelxp2j9Eg",
+  authDomain: "tama-got-u.firebaseapp.com",
+  databaseURL: "https://tama-got-u-default-rtdb.firebaseio.com",
+  projectId: "tama-got-u",
+  storageBucket: "tama-got-u.appspot.com",
+  messagingSenderId: "623580727299",
+  appId: "1:623580727299:web:6877898a72348f7c806636",
+  measurementId: "G-T081PZDCJ4",
+};
+
 function App() {
+  // Initialize Firebase
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+  }, []);
+
   return (
     <Router>
       <Switch>
