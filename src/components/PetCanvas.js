2fc17ed41de90a3c855ac23fa0b24a7cca596e45
R Lee <alpha.r.lee.dev@gmail.com>
2021-11-13 19:33:55 -0700

    Added rendering for pet

--- src/components/PetCanvas.js
+++ src/components/PetCanvas.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Canvas from "./Canvas";
 
+// Import way too many images
+import neutralStillImgSrc from "../assets/pet/neutral-still.png";
+import neutralWalkLeft0Src from "../assets/pet/neutral-walk-left-0.png";
+import neutralWalkLeft1Src from "../assets/pet/neutral-walk-left-1.png";
+import neutralWalkRight0Src from "../assets/pet/neutral-walk-right-0.png";
+import neutralWalkRight1Src from "../assets/pet/neutral-walk-right-1.png";
+
+import happyStillImgSrc from "../assets/pet/happy-still.png";
+import happyWalkLeft0Src from "../assets/pet/happy-walk-left-0.png";
+import happyWalkLeft1Src from "../assets/pet/happy-walk-left-1.png";
+import happyWalkRight0Src from "../assets/pet/happy-walk-right-0.png";
+import happyWalkRight1Src from "../assets/pet/happy-walk-right-1.png";
+
 function PetCanvas(props) {
+  const [images, setImages] = useState();
+
+  const buildImages = () => {
+    const imgs = {
+      neutral: {
+        still: [],
+        walkLeft: [],
+        walkRight: [],
+      },
+      happy: {
+        still: [],
+        walkLeft: [],
+        walkRight: [],
+      },
+    };
+
+    const imageSrcs = {
+      neutral: {
+        still: [neutralStillImgSrc],
+        walkLeft: [neutralWalkLeft0Src, neutralWalkLeft1Src],
+        walkRight: [neutralWalkRight0Src, neutralWalkRight1Src],
+      },
+      happy: {
+        still: [happyStillImgSrc],
+        walkLeft: [happyWalkLeft0Src, happyWalkLeft1Src],
+        walkRight: [happyWalkRight0Src, happyWalkRight1Src],
+      },
+    };
+
+    Object.entries(imageSrcs).forEach(([petState, petStateVal]) => {
+      Object.entries(petStateVal).forEach(([petAction, petActionVal]) => {
+        imgs[petState][petAction] = petActionVal.map((petActionSrc) => {
+          const img = new Image();
+          img.src = petActionSrc;
+          return img;
+        });
+      });
+    });
+
+    setImages(imgs);
+  };
+
   const draw = (ctx, frameCount) => {
+    if (!images) return;
+
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fillStyle = "#000000";
     ctx.beginPath();
     ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
     ctx.fill();
+
+    ctx.drawImage(images.neutral.still[0], 100, 100);
   };
+
+  useEffect(() => {
+    buildImages();
+  }, []);
 
   // Set canvas to be fullscreen
   const canvasWidth = window.innerWidth;
