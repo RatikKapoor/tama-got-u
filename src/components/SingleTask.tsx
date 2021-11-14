add8721dfafbb409b72be843a5e8abadcc5fe721
anmolahuja1 <anmol.ahuja1@ucalgary.ca>
2021-11-14 00:11:34 -0700

    made things look pretty

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -5,7 +5,7 @@ import { SingleTaskModel } from "../models/singleTask";
 const SingleTask: React.FC<SingleTaskModel> = (props: SingleTaskModel) => {
   return (
     <>
-      <ListItem sx={{ backgroundColor: "#fff", borderRadius: 10, marginBottom: 1 }}>
+      <ListItem sx={{ backgroundColor: "#fff", borderRadius: '16px', marginBottom: 1, alignSelf: 'center'}}>
         <ListItemText
           primary={props.task}
           secondary={props.nextTime.toDate().toLocaleTimeString()}
